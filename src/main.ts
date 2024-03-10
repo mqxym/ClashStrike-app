e96bd2baaba26b53b9bdb5dde1b4d48ce3b41679
mQxYm <maxim@omg.lol>
2024-03-10 18:25:58 +0100

    angular pwa

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // Make sure the path is correct
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core'; // Make sure the path is correct
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes),
-  provideAnimations(), provideAnimationsAsync()]
+    provideAnimations(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 }).catch(err => console.error(err));
