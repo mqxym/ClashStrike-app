43bc100702af14426e1a9077c1735ded9a26e6f3
mQxYm <maxim@omg.lol>
2024-05-10 15:35:35 +0200

    init app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Route } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 
 
 
 export const routes: Route[] = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to /home
-  { path: 'home', component: HomeComponent }, // Main page route
-  { path: 'timer', component: TimerComponent},
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '/home' }, // Wildcard route to redirect any unmatched paths to /home
+  { path: 'timer', component: TimerComponent }, // Main page route
+  { path: 'accounts', component: TimerComponent},
+  { path: 'overview', component: TimerComponent },
+  { path: '**', redirectTo: '/overview' }, // Wildcard route to redirect any unmatched paths to /home
 ];
