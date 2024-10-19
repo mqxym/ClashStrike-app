3e7513d742e2e772d474ac418f2a52dd146890a8
mQxYm <maxim@omg.lol>
2024-10-19 20:25:23 +0200

    Unfinished design update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Route } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 
 
 export const routes: Route[] = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to /home
   { path: 'timer', component: TimerComponent }, // Main page route
-  { path: 'accounts', component: TimerComponent},
+  { path: 'accounts', component: AccountsComponent},
   { path: 'overview', component: TimerComponent },
   { path: '**', redirectTo: '/overview' }, // Wildcard route to redirect any unmatched paths to /home
 ];
